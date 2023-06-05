90c4bc5e8bf4e589453680d188e19668aaefd098
Adomelism <adomelism@gmail.com>
2023-06-05 23:08:06 +0300

    newsItem dynamic element created

--- src/Components/NewsPageComponents/NewsItem.js
+++ src/Components/NewsPageComponents/NewsItem.js
@@ -1,15 +1,30 @@
 
 const NewsItem = ({url, imgSrc, imgAlt, category, title, date}) => {
+
+    if (!title || !url) {
+        return '';
+    }
+
+    const categoryElement = category && <span className="news-category">{category}</span>;
+    const dateElement = date && <span className="news-date">{date} </span>;
+    const titleElement =  <h3 className="news-title">{title}</h3>;
+    const imageElement = imgSrc && (
+        <div className="image-wrapper"> 
+        <img src={imgSrc} alt={imgAlt}/>
+        </div>
+    );
+
   return (
-    <div className="news-item"> <a href={url}>
-    <div className="image-wrapper"> <img
-            src={imgSrc} alt={imgAlt}/>
+    <div className="news-item"> 
+        <a href={url}>
+        {imageElement}
+        <div className="news-content"> 
+            {categoryElement}
+            {titleElement}
+            {dateElement}
+        </div>
+        </a> 
     </div>
-    <div className="news-content"> <span className="news-category">{category}</span>
-        <h3 className="news-title">{title}</h3> <span
-            className="news-date">{date}</span>
-    </div>
-</a> </div>
   )
 }
 
