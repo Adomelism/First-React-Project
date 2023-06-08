457fca1914855bf8c30e7a48b36af1379bcb9eda
Adomelism <adomelism@gmail.com>
2023-06-08 10:01:47 +0300

    push

--- src/Components/CitiesComponents/CityItem.js
+++ src/Components/CitiesComponents/CityItem.js
@@ -1,10 +1,33 @@
 
 const CityItem = ({city}) => {
-    let {name, population, location, touristAttraction, isCapital} = city;
+    let {name, population, location, touristAttractions, isCapital} = city;
+    let title = name;
+    let capitalText = '';
+    let capitalClass = '';
+    let touristAttractionText = '';
+
+    if (isCapital) {
+      title = name + ' (Capital)';
+      capitalText = name + ' is capital of ' + location.country + '.';
+      capitalClass = 'capital'
+    }
+
+console.log(touristAttractions.length)
+
+    if (touristAttractions.length == 1) {
+      touristAttractionText = 'Main Tourist attraction of ' + {name} + ' is:'
+    } else if (touristAttractions.length > 1) {
+      touristAttractionText = 'Main Tourist attractions of ' + {name} + ' are:'
+    } else {
+      touristAttractionText = '';
+    }
+
+
   return (
 <div>
-    <h2>{name}</h2>
+    <h2 className={capitalClass}>{title}</h2>
     <p>{name} is located in {location.continent} and has population of {population} people.</p>
+    <span>{capitalText}</span>
     <h3>Main Tourist attraction of {name} is:</h3>
     
 </div>
