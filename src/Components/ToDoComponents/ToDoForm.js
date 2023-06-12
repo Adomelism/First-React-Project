5c8e7bce785b5d99b779d6382c47f10bf4f13491
Adomelism <adomelism@gmail.com>
2023-06-12 11:28:47 +0300

    pre on-submit handler

--- src/Components/ToDoComponents/ToDoForm.js
+++ src/Components/ToDoComponents/ToDoForm.js
@@ -1,22 +1,36 @@
+import { useState } from "react";
 
 const ToDoForm = () => {
+
+    let [name, setName] = useState('')
+    let [description, setDescription] = useState('')
+    let [isDone, setIsDone] = useState(false)
+    let [dueDate, setDueDate] = useState('')
+
+    const nameHandler = (event) => setName(event.target.value)
+    const descriptionHandler = (event) => setDescription(event.target.value)
+    const isDoneHandler = (event) => setIsDone(event.target.checked)
+    const dueDateHandler = (event) => setDueDate(event.target.value)
+
+    // const addNewTaskHandler = 
+
   return (
     <form>
     <div>
         <label htmlFor="TaskName">Task Name: </label>
-        <input type="text" id="TaskName" name="TaskName"></input>
+        <input type="text" id="TaskName" name="TaskName" value={name} onChange={nameHandler}></input>
     </div>
     <div>
         <label htmlFor="TaskDescription">Task Description: </label>
-        <textarea id="TaskDescription" name="TaskDescription" />
+        <textarea id="TaskDescription" name="TaskDescription" value={description} onChange={descriptionHandler} />
     </div>
     <div>
         <label htmlFor="isTaskDone">Task Done: </label>
-        <input type="checkbox" id="isTaskDone" name="isTaskDone" />
+        <input type="checkbox" id="isTaskDone" name="isTaskDone" checked={isDone} onChange={isDoneHandler}/>
     </div>
     <div>
         <label htmlFor="taskDueDate">Task Due Date: </label>
-        <input type="date" id="taskDueDate" name="taskDueDate" />
+        <input type="date" id="taskDueDate" name="taskDueDate" value={dueDate} onChange={dueDateHandler}/>
     </div>
     <div>
         <button type="submit" id="submit" name="submit">Create Task</button>
