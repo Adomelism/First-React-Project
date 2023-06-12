325c60d903f49ad5dd38965775d907704d603fd0
Adomelism <adomelism@gmail.com>
2023-06-12 10:51:05 +0300

    pre-split to new component

--- src/Components/ToDoComponents/ToDoList.js
+++ src/Components/ToDoComponents/ToDoList.js
@@ -3,39 +3,47 @@ const ToDoList = () => {
 
     const tasksList = [
         {
-            task: 'laundry',
+            taskName: 'laundry',
             description: 'make your parents proud',
             isDone: false,
             dueDate: '06/13/2023',
         },
         {
-            task: 'car wash',
+            taskName: 'car wash',
             description: 'bathtime for your vehicle',
-            isDone: false,
+            isDone: true,
             dueDate: '06/14/2023',
         },
         {
-            task: 'shopping',
+            taskName: 'shopping',
             description: 'buy lasagna ingredients',
             isDone: false,
             dueDate: '06/15/2023',
         }
     ]
 
-    const isItDone = ''
-    
-    if (isDone) {
+  return (
+    <div>
+        {tasksList.map((task) => {
+        let isItDone = '';
+        let {taskName, description, isDone, dueDate} = task;
+
+        console.log(taskName)
+        console.log(description)
         console.log(isDone)
+        console.log(dueDate)
+
+         if (isDone) {
         isItDone = 'Task was completed.'
     } else {
         isItDone = 'Task is not yet completed.'
     }
-
-  return (
-    <div>
-         <h1>{task}</h1>
-        <h2>You need to {description} by {dueDate}</h2>
-        <h3>{isItDone}</h3>
+    return <div>
+                <h3>{taskName}</h3>
+                <h4>You need to {description} by {dueDate}</h4>
+                <h5>{isItDone}</h5>
+            </div>
+    })}
     </div>
    
   )
