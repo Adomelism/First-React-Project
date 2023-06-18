0d4cf990b2f5efc32a52e3cc7ace96afc72e44e4
Adomelism <adomelism@gmail.com>
2023-06-18 19:49:51 +0300

    axios

--- src/Components/AiComponents/AiForm.js
+++ src/Components/AiComponents/AiForm.js
@@ -1,20 +1,22 @@
 import { useState } from "react"
 
-const AiForm = ({nameFormHandler}) => {
+const AiForm = ({onNameSubmit}) => {
 
     const [name, setName] = useState('')
 
     const nameHandler = event => setName(event.target.value)
 
-    const formSubmitHandler = event => event.preventDefault();
-    nameFormHandler(name);
+    const formSubmitHandler = event => {
+      event.preventDefault();
+      onNameSubmit(name);
     setName('');
+    }
 
   return (
     <div>
          <form onSubmit={formSubmitHandler}>
             <label htmlFor='search-name'>Enter a name:</label>
-            <input onChange={nameHandler} type='text' id='search-name'></input>
+            <input type='text' id='search-name' value={name} onChange={nameHandler}></input>
             <button type='submit'>Search</button>
         </form>
     </div>
--- src/Pages/AiPage/AiPage.js
+++ src/Pages/AiPage/AiPage.js
@@ -1,27 +1,40 @@
-// import { useState } from 'react';
 import { useState } from 'react';
 import AiForm from '../../Components/AiComponents/AiForm';
 import AiOutput from '../../Components/AiComponents/AiOutput';
 import Container from '../../Components/Container/Container';
+import axios from 'axios';
 
 const AiPage = () => {
 
-    const [information, setInformation] = useState(null)
+    const [information, setInformation] = useState(null);
 
-    const nameFormHandler = async name => {
-        const ageRes = await fetch(`https://api.agify.io?name=` + name)
-        const ageData = await ageRes.json();
-        const age = ageData.age;
+    // const nameFormHandler = async name => {
+    //     const ageRes = await fetch('https://api.agify.io?name=' + name)
+    //     const ageData = await ageRes.json();
+    //     const age = ageData.age;
 
-        const nationalityRes = await fetch('https://api.nationalize.io?name=' + name)
-        const nationalityData = await nationalityRes.json();
-        const nationality = nationalityData.country[0].country_id;
+    //     const nationalityRes = await fetch('https://api.nationalize.io?name=' + name)
+    //     const nationalityData = await nationalityRes.json();
+    //     const nationality = nationalityData.country[0].country_id;
        
 
-        const genderRes = await fetch('https://api.genderize.io?name=' + name)
-        const genderData = await genderRes.json();
-        const gender = genderData.gender;
+    //     const genderRes = await fetch('https://api.genderize.io?name=' + name)
+    //     const genderData = await genderRes.json();
+    //     const gender = genderData.gender;
    
+
+    //     setInformation({name, age, gender, nationality});
+    // }
+
+    const nameFormHandler = async name => {
+
+        const ageRes = await axios.get('https://api.agify.io?name=' + name);
+        const genderRes = await axios.get('https://api.genderize.io?name=' + name);
+        const nationalityRes = await axios.get('https://api.nationalize.io?name=' + name);
+
+        const age = ageRes.data.age;
+        const gender = genderRes.data.gender;
+        const nationality = nationalityRes.data.country[0].country_id;
 
         setInformation({name, age, gender, nationality});
 
@@ -29,7 +42,7 @@ const AiPage = () => {
 
   return (
     <Container>
-        <AiForm nameFormHandler={nameFormHandler}/>
+        <AiForm onNameSubmit={nameFormHandler}/>
         <AiOutput data={information}/>
     </Container>
   )
